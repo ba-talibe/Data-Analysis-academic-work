6c4da340022086bb878d8d6956f8f466fad3b206
omarba2015@gmail.com <omarba2015@gmail.com>
2024-12-11 11:19:31 +0100

    added notebokk for laplacian map experimentations

--- M1 SD/TP-8-Laplacian-Embedding/laplacian_eigenmaps.py
+++ M1 SD/TP-8-Laplacian-Embedding/laplacian_eigenmaps.py
@@ -1,12 +1,33 @@
 from sklearn.neighbors import kneighbors_graph
+from sklearn.datasets import load_iris
+# import matplotlib.pyplot as plt
+from scipy import sparse 
 import numpy as np
 
-def laplacian_eigenmaps(X, n_neighbors, n_components, normilize=True):
+def laplacian_eigenmaps(X,y, n_neighbors, normilize=True):
+    N = X.shape[0]  
     kng = kneighbors_graph(X, n_neighbors, mode='distance')
-    kng = 0.5 * (kng + kng.T)
-    W =  kng.toarray()
-    D = np.diag(W.sum(axis=1))
+    W = 0.5 * (kng + kng.T)
+
+    #W = np.where(W > 0, 1, 0)
+    D = sparse.diags(np.asarray(W.sum(axis=1)).flatten())
 
     if normilize:
-        L = np.eye(X.shape[0]) - np.linalg.inv(D) @ W
+        L = np.eye(X.shape[0]) - D**-0.5 @ W @ D**-0.5
+    else:
+        L = D - W
+
+    [yl, YL] = sparse.linalg.eigsh(L, k=n_neighbors, which='SM')
    
+    print(YL)
+    return YL
+    # plt.figure(figsize=(8,8))
+    # plt.scatter(YL[:,2], YL[:,3], c=y[0, N], cmap=plt.cm.Set1)
+    # plt.show()
+
+   
+if __name__ == '__name__':
+    iris = load_iris()
+    X = iris.data
+    y = iris.target
+    print(laplacian_eigenmaps(X, y, 4, True))
